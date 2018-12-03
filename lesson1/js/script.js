dcfc4bd55f3219f86f0587d7002fd9182f7fd6d1
guvijur <vit_adm@mail.ru>
2018-12-03 21:34:26 +0300

    Пример

--- lesson1/js/script.js
+++ lesson1/js/script.js
@@ -7,7 +7,7 @@
     }
 }*/
 
-function Car(color, wheels, engine) {
+/*function Car(color, wheels, engine) {
     this.color = color;
     this.wheels = wheels;
     this.engine = engine;
@@ -15,3 +15,27 @@ function Car(color, wheels, engine) {
 
 let audi = new Car("blue", 4, {volume: 2.0, power: 225});
 let bmw = new Car("black", 4, {volume: 2.5, power: 335});
+*/
+
+function Vehicle() {
+    this.x = 0;
+    this.y = 0;
+    this.z = 0;
+    this.color = "white";
+    
+}
+
+Vehicle.prototype.move = function(x, y ,z) {
+    this.x = x;
+    this.y = y;
+    this.z = z;
+}
+
+function Car() {
+    Vehicle.call(this);
+}
+
+Car.prototype = Object.create(Vehicle.prototype);
+Car.prototype.constructor = Car;
+
+let audi = new Car();
