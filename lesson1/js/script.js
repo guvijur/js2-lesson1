d44604aa1242423ce15c53611e0c98e07ce8dbad
guvijur <vit_adm@mail.ru>
2018-12-03 20:59:14 +0300

    Закомментили первый объект. Создали функцию конструктор.

--- lesson1/js/script.js
+++ lesson1/js/script.js
@@ -1,8 +1,17 @@
-let audi = {
+/*let audi = {
     color: "blue",
     wheels: 4,
     engine: {
         volume: 2.0,
         power: 225
     }
+}*/
+
+function Car(color, wheels, engine) {
+    this.color = color;
+    this.wheels = wheels;
+    this.engine = engine;
 }
+
+let audi = new Car("blue", 4, {volume: 2.0, power: 225});
+let bmw = new Car("black", 4, {volume: 2.5, power: 335});
