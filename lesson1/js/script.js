d4660212329e3550133a8d252e9c996720139edb
guvijur <vit_adm@mail.ru>
2018-12-03 22:20:37 +0300

    ПОлный код

--- lesson1/js/script.js
+++ lesson1/js/script.js
@@ -41,3 +41,72 @@ Car.prototype.constructor = Car;
 let audi = new Car();
 */
 
+function Container() {
+    this.id = "";
+    this.className = "";
+    this.htmlCode = "";
+}
+
+Container.prototype.render = function() {
+    return this.htmlCode;
+};
+
+function Menu(myId, myClassName, myItems) {
+    Container.call(this);
+    this.id = myId;
+    this.className = myClassName;
+    this.items = myItems;
+}
+
+Menu.prototype = Object.create(Container.prototype);
+Menu.prototype.constructor = Menu;
+Menu.prototype.render = function() {
+let result = '<ul class="' + this.className + '" id="' + this.id + '">';
+    for(let item in this.items) {
+        if (this.items[item] instanceof MenuItem) {
+            result += this.items[item].render();
+        }
+    }
+    result += '</ul>';
+    return result;
+}
+
+function MenuItem(myHref, myName) {
+    Container.call(this);
+    //this.id = myId;
+    this.className = "menu-item";
+    this.href = myHref;
+    this.name = myName;
+}
+
+MenuItem.prototype = Object.create(Container.prototype);
+MenuItem.prototype.constructor = MenuItem;
+MenuItem.prototype.render = function() {
+    return '<li class="' + this.className + '">' + this.name + '</li>';
+}
+
+let mItem1 = new MenuItem("/", "Главная");
+let mItem2 = new MenuItem("/about", "О нас");
+let mItem3 = new MenuItem("/contacts", "Контакты");
+let mItems = {0: mItem1, 1: mItem2, 2: mItem3};
+
+let menu = new Menu("myMenu", "myClassName", mItems);
+document.write(menu.render());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
